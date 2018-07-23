048f19669e9cec9b78087e2e9bf34f31cc10046b
Akash Bijwe <mr.akashbijwe@gmail.com>
2018-07-23 18:15:09 +0530

    section-1 | Lecture 7-8

--- Dev/cfehome/src/restaurent/views.py
+++ Dev/cfehome/src/restaurent/views.py
@@ -3,6 +3,14 @@ from django.shortcuts import render
 
 # Create your views here.
 #function base view
-def home(request):
-  return HttpResponse("Hello")  
+#def home(request):
+  #return HttpResponse("Hello")  
   #return render(request, "home.html", {}) #response
+
+def home(request):
+  context = {
+    "html_var": "Test HTML TEXT from views.py",
+    "html_var2": True,
+    "some_list": ['Test', 'Test2', 'Test3', 'Test4']
+  }
+  return render(request, 'base.html', context) #respose
