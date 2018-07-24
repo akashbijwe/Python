cd020663341ca42ebb0dae5aa7c05b44c3824a75
Akash Bijwe <mr.akashbijwe@gmail.com>
2018-07-24 16:05:20 +0530

    section-3 | Lecture-15

--- Dev/cfehome/src/restaurent/models.py
+++ Dev/cfehome/src/restaurent/models.py
@@ -2,7 +2,9 @@ from django.db import models
 
 # Create your models here.
 class RestaurentLocation(models.Model):
-  name = models.CharField(max_length=120)
-  location = models.CharField(max_length=120, null=True, blank=True)
-
+  name       = models.CharField(max_length=120)
+  location   = models.CharField(max_length=120, null=True, blank=True)
+  category   = models.CharField(max_length=120, null=True, blank=True)
+  timestamp  = models.DateTimeField(auto_now_add = True)
+  updated    = models.DateTimeField(auto_now = True)
   
