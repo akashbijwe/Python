531234e8d948317683326e6318b66999b90436b0
Akash Bijwe <mr.akashbijwe@gmail.com>
2018-07-24 16:55:34 +0530

    Section-3 | Lecture-16

--- Dev/cfehome/src/cfehome/urls.py
+++ Dev/cfehome/src/cfehome/urls.py
@@ -15,13 +15,14 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from restaurent.views import HomeTemplateView
 from django.views.generic import TemplateView
+from restaurent.views import restaurent_listview
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', HomeTemplateView.as_view()),
-    url(r'^home/$', HomeTemplateView.as_view()),
+    url(r'^$', TemplateView.as_view(template_name='home.html')),
+    url(r'^home/$', TemplateView.as_view(template_name='home.html')),
+    url(r'^restaurent/$', restaurent_listview),
     url(r'^contact/(?P<id>\d+)/$', TemplateView.as_view(template_name='contact.html')),
     url(r'^about/$', TemplateView.as_view(template_name='about.html'))
 ]
--- Dev/cfehome/src/restaurent/views.py
+++ Dev/cfehome/src/restaurent/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from django.views import View
 from django.views.generic import TemplateView
 
+from .models import RestaurentLocation
+
 # Create your views here.
 #function base view
 #def home(request):
@@ -27,15 +29,23 @@ from django.views.generic import TemplateView
 # def about(request):
 #   return render(request, 'about.html') #respose
 
-class HomeTemplateView(TemplateView):
-  template_name = 'home.html'
+# class HomeTemplateView(TemplateView):
+#   template_name = 'home.html'
 
-  def get_context_data(self, *args, **kwargs):
-    context = super(HomeTemplateView, self).get_context_data(*args, **kwargs)
-    context = {
-      "html_var": "Test HTML TEXT from views.py",
-      "html_var2": True,
-      "some_list": ['Test', 'Test2', 'Test3', 'Test4']
-    }
-    print(context)
-    return context
+#   def get_context_data(self, *args, **kwargs):
+#     context = super(HomeTemplateView, self).get_context_data(*args, **kwargs)
+#     context = {
+#       "html_var": "Test HTML TEXT from views.py",
+#       "html_var2": True,
+#       "some_list": ['Test', 'Test2', 'Test3', 'Test4']
+#     }
+#     print(context)
+#     return context
+
+def restaurent_listview(request):
+  template_name = 'restaurent/restaurents_list.html'
+  queryset  = RestaurentLocation.objects.all()
+  context = {
+    "object_list": queryset
+  }
+  return render(request, template_name, context)
