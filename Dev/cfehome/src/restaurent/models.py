2f5b0938828216b2e0bd63591b1c53386050f34c
Akash Bijwe <mr.akashbijwe@gmail.com>
2018-07-24 15:35:08 +0530

    Section-3 | Lecture-14

--- Dev/cfehome/src/restaurent/models.py
+++ Dev/cfehome/src/restaurent/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class RestaurentLocation(models.Model):
+  name = models.CharField(max_length=120)
+  location = models.CharField(max_length=120, null=True, blank=True)
+
+  
